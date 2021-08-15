cfb8f00358c953198d4aaf7d78aed95ec84076c5
Hu Rui <hr101191@gmail.com>
2021-08-15 15:57:16 +0800

    Bypass TLS when connecting to ldap server

--- keycloak/keycloak-auth-server/src/main/java/com/hurui/keycloak/EmbeddedKeycloakApplication.java
+++ keycloak/keycloak-auth-server/src/main/java/com/hurui/keycloak/EmbeddedKeycloakApplication.java
@@ -1,7 +1,12 @@
 package com.hurui.keycloak;
 
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.NoSuchElementException;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.ApplicationPath;
 import org.keycloak.Config;
 import org.keycloak.services.resources.KeycloakApplication;
@@ -20,6 +25,35 @@ public class EmbeddedKeycloakApplication extends KeycloakApplication {
 	
 	public EmbeddedKeycloakApplication() {
 		super();
+		//bypass TLS when working with ldaps
+		trustSelfSignedSSL();
+	}
+	
+	public static void trustSelfSignedSSL() {
+		try {
+			SSLContext sslContext = SSLContext.getInstance("TLS");
+			X509TrustManager x509TrustManager = new X509TrustManager() {
+				
+				@Override
+				public X509Certificate[] getAcceptedIssuers() {
+					return null;
+				}
+				
+				@Override
+				public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+
+				}
+				
+				@Override
+				public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+
+				}
+			};
+			sslContext.init(null, new TrustManager[] {x509TrustManager}, null);
+			SSLContext.setDefault(sslContext);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
